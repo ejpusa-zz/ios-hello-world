13a2ca6da5c864481f1692e7a2cacf8cf19f6197
Edward Potter <edwardpotter@gmail.com>
2015-11-07 09:26:01 -0500

    Initial Commit

---
+++ gittester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittester
+//
+//  Created by Edward Potter on 11/7/15.
+//  Copyright (c) 2015 Edward Potter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
