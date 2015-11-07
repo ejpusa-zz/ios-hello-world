8c43afe34143239c57cb5d8f9e769b095b0c462d
Edward Potter <edwardpotter@gmail.com>
2015-11-07 09:34:30 -0500

    clean

--- gittester/ViewController.swift
+++ gittester/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-    print("viewDidLoad")
+    print("viewDidLoad-new")
   }
 
   override func didReceiveMemoryWarning() {
