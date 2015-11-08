9916cb71e438db9b9706039f1b425516b9f56404
Edward Potter <edwardpotter@gmail.com>
2015-11-08 12:39:33 -0500

    Cleaned code. Even simpler

--- gittester/ViewController.swift
+++ gittester/ViewController.swift
@@ -14,14 +14,14 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-    print("viewDidLoad-masterBranch")
+    print("viewDidLoad")
+    
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
 
 }
 
